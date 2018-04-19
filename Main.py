21d5af1e1a7d2e8baa2c05bbdbff33ec11a50839
Rennan Rocha <rennanfreitas11@gmail.com>
2018-04-19 15:09:02 -0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import *
+from ev3dev.ev3 import ColorSensor, INPUT_1, INPUT_2, OUTPUT_A, OUTPUT_B, LargeMotor
 from PID import PID
 
 VALUE_ON_WHITE = 100
@@ -21,36 +21,37 @@ assert m_right.connected
 cl_left.mode = 'COL-REFLECT'
 cl_right.mode = 'COL-REFLECT'
 
-offset = 0
+offset = -9
 
 def run(kp, ki, kd, TP):
 	pid = PID(kp, ki, kd)
 	pid.SetPoint=0.0
 
 	arq = open('log.txt', 'w+')
-	print("erro,potencia esquerda,potencia direita")
+	print("erro,sensor esquerdo,sensor direito")
 	while True:
 		error = (cl_left.value() - cl_right.value()) - offset
 
-        # u < 0 : Sensor direito encostando na linha
-        # u > 0 : Sensor esquerdo encostando na linha
+        # u > 0 : Sensor direito encostando na linha
+        # u < 0 : Sensor esquerdo encostando na linha
         # u = 0 : Ambos os sensores na linha, ou nenhum na linha
 
         #u = kp * error  # Ganho proporcional ao erro
-		
+	
 		pid.update(error)
 
 		u = pid.output
 
 		#arq.write('tp-u=%i  tp+u=%i\n' % (TP-u, TP+u))
 
-		l = saturar(TP - u)
-		r = saturar(TP + u)
+		l = saturar(TP + u)
+		r = saturar(TP - u)
 
-		print(u, l, r, sep=",")
+		print(error, sep=",")
 
 		m_left.run_forever(speed_sp=l)
 		m_right.run_forever(speed_sp=r)
+
 
 def saturar(tp):
 	if(tp > 1000):
